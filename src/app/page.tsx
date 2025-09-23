a9c9f7a473eaa02e72b588e44aa4a3f6e1e37ccd
Baljot Chohan <baljotchohan23@gmail.com>
2025-09-23 10:53:12 +0000

    remove home page direct land on services

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
-import { Faq } from '@/components/sections/faq';
-import { HeroSection } from '@/components/sections/hero-section';
 import { ServicesOverview } from '@/components/sections/services-overview';
-import { Testimonials } from '@/components/sections/testimonials';
 
 export default function Home() {
   return (
     <>
-      <HeroSection />
       <ServicesOverview />
-      <Testimonials />
-      <Faq />
     </>
   );
 }
