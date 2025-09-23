0e2e832fad720db309499166d3e17742c59be591
Baljot Chohan <baljotchohan23@gmail.com>
2025-09-23 13:23:24 +0000

    make it transpret like a glass

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export function Header() {
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b border-white/10 bg-black/20 backdrop-blur-lg">
       <div className="container flex h-14 max-w-screen-2xl items-center px-4">
         <div className="mr-4 flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
