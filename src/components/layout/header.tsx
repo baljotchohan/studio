ddffbba83c8024901d616d1ec843bf99938bfd34
Baljot Chohan <baljotchohan23@gmail.com>
2025-09-23 10:23:08 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,9 +1,9 @@
+
 "use client";
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState, useEffect } from "react";
-import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
 const navLinks = [
