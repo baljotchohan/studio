4ce8aa89d0790d5321d03712a3c9ac88f5dc9a3f
Baljot Chohan <baljotchohan23@gmail.com>
2025-09-23 10:47:18 +0000

    remove footer section from every page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Header } from '@/components/layout/header';
-import { Footer } from '@/components/layout/footer';
 import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
 
@@ -26,7 +25,6 @@ export default function RootLayout({
         <div className="relative flex min-h-dvh flex-col bg-background">
           <Header />
           <main className="flex-1">{children}</main>
-          <Footer />
         </div>
         <Toaster />
       </body>
